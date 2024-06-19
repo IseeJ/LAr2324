467d9460ddf2faa0988d6cbab3268481eb95151a
IseeJ <122057875+IseeJ@users.noreply.github.com>
2024-06-19 09:14:05 -0400

    Update purifier.py

--- Purity/purifier.py
+++ Purity/purifier.py
@@ -3,6 +3,13 @@ import json
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtCore import Qt
 
+
+import sys
+import time
+from datetime import datetime
+import serial
+
+
 #from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 
@@ -10,6 +17,8 @@ from mainwindow import Ui_MainWindow
 
 import random
 
+
+"""
 class PurifierModel(QtCore.QAbstractListModel):
     def __init__(self, *args, **kwargs):
         super(PurifierModel, self).__init__(*args, **kwargs)
@@ -24,82 +33,87 @@ class PurifierModel(QtCore.QAbstractListModel):
 
     def reset(self):
         self.data = []
-
+"""
         
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-
-        # setup user interface from Designer
-        # see: https://www.riverbankcomputing.com/static/Docs/PyQt5/designer.html
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        
-        self.model = PurifierModel()
-
-        # initialize the graphs
-        self.initGraph()
-
-        # connect signals/slots
-        self.ui.startStopButton.pressed.connect(self.toggleRun)
-        self.ui.clearButton.pressed.connect(self.clearPlot)
-
-        # setup a timer (for updating the plot).
-        # eventually, the timer would be replaced by the arrival of serial data
-        # which would trigger a plot update
+    def __init__(self, serial_port, baud_rate):
+        super().__init__()
+        self.ser = serial.Serial(serial_port, baud_rate, timeout=10)
+        self.plot_graph = pg.PlotWidget()
+        self.setCentralWidget(self.plot_graph)
+        self.plot_graph.setBackground("w")
+        pen_T1 = pg.mkPen(color=(255, 0, 0))
+        pen_T2 = pg.mkPen(color=(0, 0, 255))
+        self.plot_graph.setTitle("Temperature vs Time", color="b", size="20pt")
+        styles = {"color": "red", "font-size": "18px"}
+        self.plot_graph.setLabel("left", "Temperature (°C)", **styles)
+        self.plot_graph.setLabel("bottom", "Time (min)", **styles)
+        self.plot_graph.addLegend()
+        self.plot_graph.showGrid(x=True, y=True)
+        self.plot_graph.setYRange(20, 40)
+        self.time = []
+        self.temperature_T1 = []
+        self.temperature_T2 = []
+        self.start_time = datetime.now()
+        self.line_T1 = self.plot_graph.plot(
+            self.time,
+            self.temperature_T1,
+            name="T1",
+            pen=pen_T1,
+            symbol="o",
+            symbolSize=15,
+            symbolBrush="r",
+        )
+        self.line_T2 = self.plot_graph.plot(
+            self.time,
+            self.temperature_T2,
+            name="T2",
+            pen=pen_T2,
+            symbol="o",
+            symbolSize=15,
+            symbolBrush="b",
+        )
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(300) # ms
-        self.timer.timeout.connect(self.update)
-        
-    def initGraph(self):
-        self.ui.graphWidget.setBackground("w")
-
-        # define color for line on graph
-        pen = pg.mkPen(color=(255, 0, 0))
-        
-        self.line = self.ui.graphWidget.getPlotItem().plot(pen=pen)
-        self.plotData()
-
-        self.ui.graphWidget.setLabel("left", "y-values [arb.]")
-        self.ui.graphWidget.setLabel("bottom", "Time [arb.]")
-
-        self.runningFlag = False
-
-    def clearPlot(self):
-        self.model.reset()
-        self.plotData()
-        
-    def toggleRun(self):
-        if self.runningFlag:
-            self.stopRun()
-        else:
-            self.startRun()
-
-    def startRun(self):
-        print('hi')
-        self.runningFlag = True
-        #call addPoint() on a timer
+        self.timer.setInterval(300)
+        self.timer.timeout.connect(self.update_plot)
         self.timer.start()
-        
-    def stopRun(self):
-        print('bye')
-        self.runningFlag = False
-        # kill the addPoint() timer
-        self.timer.stop()
 
-    def addPoint(self):
-        self.model.appendData(random.randint(0,10))
+    def update_plot(self):
+        hex_data = [0x01, 0x16, 0x7B, 0x28, 0x48, 0x4C, 0x45, 0x48, 0x54, 0x43, 0x34, 0x30, 0x39, 0x35, 0x67, 0x71, 0x29, 0x7D, 0x7E, 0x04]
+        byte_data = bytearray(hex_data)
+        self.ser.write(byte_data)
+        time.sleep(2)
+        response = self.ser.read(37)
+        response_hex = response.hex()
+        bits_response = ' '.join(response_hex[i:i+2] for i in range(0, len(response_hex), 2))
+        pairs = bits_response.split()
+        print(response_hex)
+        #T1
+        pair_18 = pairs[17]  # 18th bit
+        pair_17 = pairs[16]  # 17th bit
+        T1 = pair_18 + pair_17
+        T1_d = int(T1, 16) / 10
 
-    def update(self):
-        self.addPoint()
-        self.plotData()
+        #T2
+        pair_20 = pairs[19]  # 20th bit
+        pair_19 = pairs[18]  # 19th bit
+        T2 = pair_20 + pair_19
+        T2_d = int(T2, 16) / 10
 
-    def plotData(self):
-        self.line.setData(range(len(self.model.data)), self.model.data)
+        print("T1:", T1, T1_d, "T2:", T2, T2_d)
+        current_time = (datetime.now() - self.start_time).seconds/60
+        self.time.append(current_time)
+        self.temperature_T1.append(T1_d)
+        self.temperature_T2.append(T2_d)
+
+        self.line_T1.setData(self.time, self.temperature_T1)
+        self.line_T2.setData(self.time, self.temperature_T2)
         
         
 app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
+serial_port = '/dev/cu.usbserial-110'
+baud_rate = 38400
+MainWindow(serial_port, baud_rate)
 window.show()
 app.exec_()
 
